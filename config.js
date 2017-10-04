bbe6bcc1e55d146529d546b011d4c79c1e5e408f
David Le <davidhle721@gmail.com>
2017-10-04 17:27:00 -0400

    add inital files and tokens/keys

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'J6heXUYQaefd0h3tGNEOWbkfB',
+  consumer_secret:      'iT4HG3uwtYafZbkp08W38uV2Fi8okneyqE0nuKh6sSLDNyt29D',
+  access_token:         '915680496964784133-01qL5bH7QZOHaIAdTpm5cl4Z3XFZsxl',
+  access_token_secret:  '3S2SsmKEhCx5lQ9j56ZQWCAEJeBnyzrDIKRreIu4jm9bS'
+}
